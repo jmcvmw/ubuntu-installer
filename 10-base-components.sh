2d946f083ada26dd1a5f3f835912a409ce51d133
Jason Meers <jason@jasonmeers.com>
2026-09-01 22:58:02 +0100

    added base componenets2

---
+++ 10-base-components.sh
@@ -0,0 +1,30 @@
+sudo apt install openssh-server -y
+sudo vfw allow ssh
+sudo systemctl enable ssh
+sudo systemctl start ssh
+sudo systemctl status ssh
+
+sudo apt install wget -y
+sudo apt install curl -y
+
+
+sudo apt install lm-sensors -y
+sudo apt install htop -y
+sudo apt install btop -y
+sudo apt install iftop -y
+sudo apt install iotop -y
+sudo apt install net-tools -y
+sudo apt install smartmontools -y
+sudo apt install inxi -y
+
+
+sudo apt install gh -y
+sudo apt install vim -y
+sudo apt install openssl -y
+sudo apt install libssl-dev -y
+sudo apt install cmake -y
+sudo apt install nano -y
+
+
+sudo apt install gcc -y
+sudo apt install hipcc -y
